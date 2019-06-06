b2b5c3d36babe3883be6b02860e521af0e2579d4
hordydmitry <hordydmitry@gmail.com>
2019-06-06 11:10:01 +0500

    hw1

---
+++ hw_lesson_1/scripts/hw1/hw1-task4.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function task3() {
+    
+    let mode = "normal";  
+    let my_valu3 = 102; 
+   // let 3my_value3 = "102"; // Название переменной не может начинаться с цифры
+    let __hello__ = "world"; 
+    let $$$ = "money"; 
+   // let !0_world = true; // Название переменной не может начинаться с !
+}
